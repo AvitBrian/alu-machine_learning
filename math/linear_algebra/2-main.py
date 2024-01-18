1af1298d4c3b5e7d3dbbda90ab7759af4a403a53
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-18 14:33:49 +0000

    updates

--- math/linear_algebra/2-main.py
+++ math/linear_algebra/2-main.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python3
+"""
+Module for calculating the shape of a nested list.
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
+This module provides a function to calculate the shape of a nested list,
+representing a multi-dimensional matrix.
 
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
+Example:
+    mat = [[1, 2], [3, 4]]
+    shape = matrix_shape(mat)
+    print(shape)
+
+    mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+            [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+    shape2 = matrix_shape(mat2)
+    print(shape2)
+"""
+
+
+def matrix_shape(matrix):
+    """
+    Calculate the shape of a nested list.
+
+    Args:
+        matrix (list): The input nested list representing a multi-dimensional matrix.
+
+    Returns:
+        list: The shape of the matrix.
+    """
+    shape = []
+    while type(matrix) == list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python3
+"""
+Module for calculating the shape of a nested list.
+
+This module provides a function to calculate the shape of a nested list,
+representing a multi-dimensional matrix.
+
+Example:
+    mat = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+    shape = matrix_shape(mat)
+    print(shape)
+"""
+
+
 def matrix_shape(matrix):
+    """
+    Calculate the shape of a nested list.
+
+    Args:
+        matrix (list): The input nested list representing a multi-dimensional matrix.
+
+    Returns:
+        list: The shape of the matrix.
+    """
     shape = []
     while type(matrix) == list:
         shape.append(len(matrix))
