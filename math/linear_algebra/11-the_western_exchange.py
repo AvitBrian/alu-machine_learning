fa6c844c2ea4d465709866fdc94e835b6d98aeda
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-18 13:58:10 +0000

    updates

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
+"""
+Module for transposing matrices using NumPy.
+
+This module provides a function, np_transpose, for transposing a matrix using
+the NumPy library.
+
+Example:
+    matrix = np.array([[1, 2, 3], [4, 5, 6]])
+    transposed_matrix = np_transpose(matrix)
+    print("Original Matrix:")
+    print(matrix)
+    print("Transposed Matrix:")
+    print(transposed_matrix)
+"""
+
 import numpy as np
 
 
 def np_transpose(matrix):
+    """
+    Transpose a matrix using NumPy.
+
+    Args:
+        matrix (numpy.ndarray): The matrix to be transposed.
+
+    Returns:
+        numpy.ndarray: The transposed matrix.
+
+    Example:
+        matrix = np.array([[1, 2, 3], [4, 5, 6]])
+        transposed_matrix = np_transpose(matrix)
+        print("Original Matrix:")
+        print(matrix)
+        print("Transposed Matrix:")
+        print(transposed_matrix)
+    """
     return matrix.T
--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
+"""
+Module for element-wise matrix operations using NumPy.
+
+This module provides a function, np_elementwise, for performing element-wise
+addition, subtraction, multiplication, and division on two matrices using the
+NumPy library.
+
+Example:
+    mat1 = np.array([[1, 2], [3, 4]])
+    mat2 = np.array([[5, 6], [7, 8]])
+    add, sub, mul, div = np_elementwise(mat1, mat2)
+    print("Addition:", add)
+    print("Subtraction:", sub)
+    print("Multiplication:", mul)
+    print("Division:", div)
+"""
+
 import numpy as np
 
 
 def np_elementwise(mat1, mat2):
-    """ Matrix Operations
+    """
+    Perform element-wise matrix operations using NumPy.
 
     Args:
-        mat1 (_type_): Matrix 1
-        mat2 (_type_): Matrix 2
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
 
     Returns:
-        _type_: different Operations
+        tuple: A tuple containing the element-wise sum, difference, product,
+            and quotient, respectively.
+
+    Example:
+        mat1 = np.array([[1, 2], [3, 4]])
+        mat2 = np.array([[5, 6], [7, 8]])
+        add, sub, mul, div = np_elementwise(mat1, mat2)
+        print("Addition:", add)
+        print("Subtraction:", sub)
+        print("Multiplication:", mul)
+        print("Division:", div)
     """
     addition = mat1 + mat2
-    substraction = mat1 - mat2
+    subtraction = mat1 - mat2
     multiplication = mat1 * mat2
     division = mat1 / mat2
-    return addition, substraction, multiplication, division
+    return addition, subtraction, multiplication, division
