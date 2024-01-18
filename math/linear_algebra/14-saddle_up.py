fa4f62a2ff25991ff66739117c83bfdd0bba5dfc
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-18 13:17:53 +0000

    testing still

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 import numpy as np
 
+"""
+    Perform matrix multiplication using NumPy.
+
+    Args:
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
+
+    Returns:
+        numpy.ndarray: The result of matrix multiplication.
+        
+    Raises:
+        ValueError: If the matrices are not compatible for multiplication.
+
+    Example:
+        mat1 = np.array([[1, 2], [3, 4]])
+        mat2 = np.array([[5, 6], [7, 8]])
+        result = np_matmul(mat1, mat2)
+        print(result)
+"""
+
+
 def np_matmul(mat1, mat2):
     """
     Perform matrix multiplication using NumPy.
@@ -11,7 +32,7 @@ def np_matmul(mat1, mat2):
 
     Returns:
         numpy.ndarray: The result of matrix multiplication.
-        
+
     Raises:
         ValueError: If the matrices are not compatible for multiplication.
 
