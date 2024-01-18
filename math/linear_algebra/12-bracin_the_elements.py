439268b84ec1af6ec0d50cc061126a5e5faa7d90
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-18 12:54:10 +0000

    updates

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,6 +1,7 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 #!/usr/bin/env python3
 import numpy as np
+
 
 def np_elementwise(mat1, mat2):
     addition = mat1 + mat2
--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 def cat_arrays(arr1, arr2):
     cat_array = []
     for element in arr1:
