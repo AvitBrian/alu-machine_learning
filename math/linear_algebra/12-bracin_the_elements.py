b79340fdaf8d2d2ea5f79b15c8298079a081e946
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-19 18:13:50 +0000

    updates

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,9 +2,10 @@
 """
 Module for element-wise matrix operations.
 
-This module provides a function, np_elementwise,
+This module provides a function, np_elementwise, 
 for performing element-wise
-addition, subtraction, multiplication, and division on two matrices.
+addition, subtraction, multiplication, and division
+on two matrices.
 
 Example:
     mat1 = [[1, 2], [3, 4]]
@@ -26,8 +27,8 @@ def np_elementwise(mat1, mat2):
         mat2 (list): The second matrix.
 
     Returns:
-        tuple: A tuple containing the element-wise sum, difference, product,
-            and quotient, respectively.
+        tuple: A tuple containing the element-wise sum, 
+        difference, product, and quotient, respectively.
 
     Example:
         mat1 = [[1, 2], [3, 4]]
@@ -38,16 +39,12 @@ def np_elementwise(mat1, mat2):
         print("Multiplication:", mul)
         print("Division:", div)
     """
-    # if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
-    #     raise ValueError("Matrices must have the same dimensions.")
-
-    addition = list(map(lambda row1, row2: list(
-        map(lambda a, b: a + b, row1, row2)), mat1, mat2))
-    subtraction = list(map(lambda row1, row2: list(
-        map(lambda a, b: a - b, row1, row2)), mat1, mat2))
-    multiplication = list(map(lambda row1, row2: list(
-        map(lambda a, b: a * b, row1, row2)), mat1, mat2))
-    division = list(map(lambda row1, row2: list(
-        map(lambda a, b: a / b, row1, row2)), mat1, mat2))
-
+    addition = list(map(lambda x, y: list(
+        map(lambda a, b: a + b, x, y)), mat1, mat2))
+    subtraction = list(map(lambda x, y: list(
+        map(lambda a, b: a - b, x, y)), mat1, mat2))
+    multiplication = list(map(lambda x, y: list(
+        map(lambda a, b: a * b, x, y)), mat1, mat2))
+    division = list(map(lambda x, y: list(
+        map(lambda a, b: a / b, x, y)), mat1, mat2))
     return addition, subtraction, multiplication, division
