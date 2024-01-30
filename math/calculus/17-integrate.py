c3f1b5d43a3c66d054c5d7dbeb46596898d293ef
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-30 19:17:22 +0200

    final tweaks

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -5,28 +5,47 @@
 
 
 def poly_integral(poly, C=0):
-    if type(poly) is not list or len(poly) < 1:
-        return None
-    if type(C) is not int and type(C) is not float:
-        return None
+  """
+  Calculate the integral of a polynomial.
 
-    if len(poly) == 1:
-        return [C]
+  Args:
+    poly (list): The coefficients of the polynomial in descending order.
+    C (int or float, optional): The constant of integration. Defaults to 0.
 
-    integral = [C]
+  Returns:
+    list: The coefficients of the integral polynomial.
 
-    for i in range(len(poly)):
-        if type(poly[i]) is not int and type(poly[i]) is not float:
-            return None
+  Raises:
+    None
 
-        if i == 0:
-          integral.append(poly[i])
-        else:
-            result = poly[i] / (i + 1)
-            new_coefficient = result if result % 1 != 0 else int(result)
-            integral.append(new_coefficient)
+  Examples:
+    >>> poly_integral([1, 2, 3])
+    [0, 1, 1.0, 1.0]
+    >>> poly_integral([1, 0, 1], C=2)
+    [2, 1, 0.5, 0.3333333333333333]
+  """
+  if type(poly) is not list or len(poly) < 1:
+    return None
+  if type(C) is not int and type(C) is not float:
+    return None
 
-    while integral[-1] == 0 and len(integral) > 1:
-        integral = integral[:-1]
+  if len(poly) == 1:
+    return [C]
 
-    return integral
+  integral = [C]
+
+  for i in range(len(poly)):
+    if type(poly[i]) is not int and type(poly[i]) is not float:
+      return None
+
+    if i == 0:
+      integral.append(poly[i])
+    else:
+      result = poly[i] / (i + 1)
+      new_coefficient = result if result % 1 != 0 else int(result)
+      integral.append(new_coefficient)
+
+  while integral[-1] == 0 and len(integral) > 1:
+    integral = integral[:-1]
+
+  return integral
