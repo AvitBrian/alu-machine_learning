8c2acb06b677b3fc387010f5024377f72f9ae7ba
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-30 19:57:02 +0200

    final tweaks

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -8,6 +8,14 @@ def poly_derivative(poly):
     '''
     Returns the derivative of a polynomial
     '''
-    if not (isinstance(poly, list) | len(poly) <= 1):
+    if type(poly) is not list or not all(isinstance(coef, (int, float)) for coef in poly):
+        return None
+
+    if len(poly) == 1:
         return [0]
-    return [poly[i] * i for i in range(1, len(poly))]
+
+    derivative = []
+    for power, coef in enumerate(poly[1:], start=1):
+        derivative.append(coef * power)
+
+    return derivative
