c05719bd187aa402f0c487cf14e8c7b6f3c00dcd
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-01-30 18:51:38 +0200

    updates

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -9,15 +9,24 @@ def poly_integral(poly, C=0):
         return None
     if type(C) is not int and type(C) is not float:
         return None
+
     if len(poly) == 1:
         return [C]
+
     integral = [C]
+
     for i in range(len(poly)):
         if type(poly[i]) is not int and type(poly[i]) is not float:
             return None
-        result = poly[i] / (i + 1)
-        new_coefficient = result if result % 1 != 0 else int(result)
-        integral.append(new_coefficient)
-    while integral[-1] is 0 and len(integral) > 1:
+
+        if i == 0:  # Handle the constant term separately
+            integral.append(poly[i])
+        else:
+            result = poly[i] / (i + 1)
+            new_coefficient = result if result % 1 != 0 else int(result)
+            integral.append(new_coefficient)
+
+    while integral[-1] == 0 and len(integral) > 1:
         integral = integral[:-1]
+
     return integral
