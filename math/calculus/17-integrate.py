f2f3271d356c2e75352c44578e448d76c83cf811
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-02-02 02:12:29 +0200

    final tweaks

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,51 +1,45 @@
-#!/usr/bin/env python3
-'''
-    This function calculates the integral of a polynomial
-'''
-
-
 def poly_integral(poly, C=0):
-  """
-  Calculate the integral of a polynomial.
+    """
+    Calculate the integral of a polynomial.
 
-  Args:
-    poly (list): The coefficients of the polynomial in descending order.
-    C (int or float, optional): The constant of integration. Defaults to 0.
+    Args:
+      poly (list): The coefficients of the polynomial in descending order.
+      C (int or float, optional): The constant of integration. Defaults to 0.
 
-  Returns:
-    list: The coefficients of the integral polynomial.
+    Returns:
+      list: The coefficients of the integral polynomial.
 
-  Raises:
-    None
+    Raises:
+      None
 
-  Examples:
-    >>> poly_integral([1, 2, 3])
-    [0, 1, 1.0, 1.0]
-    >>> poly_integral([1, 0, 1], C=2)
-    [2, 1, 0.5, 0.3333333333333333]
-  """
-  if type(poly) is not list or len(poly) < 1:
-    return None
-  if type(C) is not int and type(C) is not float:
-    return None
+    Examples:
+      >>> poly_integral([1, 2, 3])
+      [0, 1, 1.0, 1.0]
+      >>> poly_integral([1, 0, 1], C=2)
+      [2, 1, 0.5, 0.3333333333333333]
+    """
+    if not isinstance(poly, list) or len(poly) < 1:
+        return None
+    if not isinstance(C, (int, float)):
+        return None
 
-  if len(poly) == 1:
-    return [C]
+    if len(poly) == 1:
+        return [C]
 
-  integral = [C]
+    integral = [C]
 
-  for coefficient in poly:
-        if type(coefficient) is not int and type(coefficient) is not float:
+    for power, coefficient in enumerate(poly):
+        if not isinstance(coefficient, (int, float)):
             return None
-        if type(C) is float and C.is_integer():
-            C = int(C)
-        integral = [C]
-        for power, coefficient in enumerate(poly):
-            if (coefficient % (power + 1)) is 0:
-                new_coefficient = coefficient // (power + 1)
-            else:
-                new_coefficient = coefficient / (power + 1)
-            integral.append(new_coefficient)
-        while integral[-1] is 0 and len(integral) > 1:
-            integral = integral[:-1]
-        return integral
+
+        if coefficient % (power + 1) == 0:
+            new_coefficient = coefficient // (power + 1)
+        else:
+            new_coefficient = coefficient / (power + 1)
+
+        integral.append(new_coefficient)
+
+    while integral[-1] == 0 and len(integral) > 1:
+        integral = integral[:-1]
+
+    return integral
