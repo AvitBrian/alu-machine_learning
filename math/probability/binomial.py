3a96bfa77abbf6e622c21e5839bee9da4371c940
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-02-10 01:09:48 +0200

    Add parameter validation and documentation to Binomial class

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -15,6 +15,19 @@ class Binomial:
     """
 
     def __init__(self, data=None, n=1, p=0.5):
+        """
+        Initializes a Binomial object.
+
+        Parameters:
+        - data (list): A list of values.
+        - n (int): The number of trials (default is 1).
+        - p (float): The probability of success (default is 0.5).
+
+        Raises:
+        - ValueError: If n is less than 1 or if p is not between 0 and 1.
+        - TypeError: If data is not a list.
+        - ValueError: If data contains less than 2 values.
+        """
         if data is None:
             if n < 1:
                 raise ValueError("n must be a positive value")
@@ -30,6 +43,17 @@ class Binomial:
             self.n, self.p = self.calculate_parameters(data)
 
     def calculate_parameters(self, data):
+        """
+        Calculates the parameters for a binomial distribution based on the given data.
+
+        Parameters:
+        data (list): A list of numerical values representing the data.
+
+        Returns:
+        tuple: A tuple containing the calculated parameters (n, p), where:
+            - n (int): The number of trials.
+            - p (float): The probability of success in each trial.
+        """
         mean = sum(data) / len(data)
         variance = sum((x - mean) ** 2 for x in data) / len(data)
         q = variance / mean
@@ -39,6 +63,15 @@ class Binomial:
         return n, p
 
     def pmf(self, k):
+        """
+        Calculates the probability mass function (PMF) of the binomial distribution for a given value of k.
+
+        Parameters:
+        - k (int): The number of successes in the binomial distribution.
+
+        Returns:
+        - pmf (float): The probability mass function value for the given value of k.
+        """
         k = int(k)
         if k < 0:
             return 0
@@ -51,6 +84,16 @@ class Binomial:
         return pmf
 
     def cdf(self, k):
+        """
+        Calculates the cumulative distribution function (CDF) of the binomial distribution.
+
+        Parameters:
+        - k (int): The number of successes.
+
+        Returns:
+        - cdf (float): The cumulative probability of getting k or fewer successes.
+
+        """
         k = int(k)
         if k < 0:
             return 0
