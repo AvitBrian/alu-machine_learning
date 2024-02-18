4c834127be3f238b41f1811ce757f80a7ff2d9ba
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-02-18 22:09:01 +0000

    updates

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -41,6 +41,7 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     for i in range(output_h):
         for j in range(output_w):
             convolved_images[:, i, j] = np.sum(
-                padded_images[:, i*sh:i*sh + kh,j*sw:j*sw + kw] * kernel, axis=(1, 2))
+                padded_images[:, i*sh:i*sh + kh,
+                              j*sw:j*sw + kw] * kernel, axis=(1, 2))
 
     return convolved_images
