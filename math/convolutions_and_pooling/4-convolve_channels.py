280772618e301b150dd9886e0058fdac5a4100ef
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-02-18 23:16:30 +0000

    updates

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-this function convolution on images with channels and various options.
+this function does convolution on images with channels and various options.
 """
 
 import numpy as np
@@ -26,16 +26,16 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     if padding == 'valid':
         ph, pw = 0, 0
     elif padding == 'same':
-        ph = ((((h - 1) * sh) + kh - h) // 2) + 1
-        pw = ((((w - 1) * sw) + kw - w) // 2) + 1
+        ph = (((h - 1) * sh) + kh - h) // 2
+        pw = (((w - 1) * sw) + kw - w) // 2
     else:
         ph, pw = padding
 
     padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                            'constant', 0)
 
-    output_h = (h - kh + 2 * ph) // sh + 1
-    output_w = (w - kw + 2 * pw) // sw + 1
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
     convolved_images = np.zeros((m, output_h, output_w))
 
     for i in range(output_h):
