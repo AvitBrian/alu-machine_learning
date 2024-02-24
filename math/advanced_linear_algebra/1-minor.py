18a358045b4a95a67419dc24241246a5f0def6e5
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-02-24 17:43:44 +0200

    updates!

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -19,29 +19,22 @@ def minor(matrix):
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
-    num_rows = len(matrix)
-    if num_rows == 0 or any(len(row) != num_rows for row in matrix):
+    if not matrix or len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a non-empty square matrix")
 
-    def determinant(mat):
-        """
-        Calculates the determinant of a matrix.
-        """
-        if len(matrix) == 1:
-            return matrix[0][0]
-        if len(matrix) == 2:
-            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-        det = 0
-        for j in range(num_rows):
-            det += ((-1) ** j) * matrix[0][j] * determinant(
-                [row[:j] + row[j + 1:] for row in matrix[1:]])
-        return det
+    n = len(matrix)
+    minor_matrix = []
 
-    minor_mat = []
-    for i in range(num_rows):
+    for i in range(n):
         minor_row = []
-        for j in range(num_rows):
-            submatrix = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
-            minor_row.append(determinant(submatrix))
-        minor_mat.append(minor_row)
-    return minor_mat
+        for j in range(n):
+            minor_row.append(matrix[(i + 1) % n][(j + 1) % n] *
+                             matrix[(i + 2) % n][(j + 2) % n] -
+                             matrix[(i + 1) % n][(j + 2) % n] *
+                             matrix[(i + 2) % n][(j + 1) % n])
+        minor_matrix.append(minor_row)
+
+    return minor_matrix
+
+
+print(minor([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
