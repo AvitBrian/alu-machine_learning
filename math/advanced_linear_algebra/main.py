09aee73793a2f37f07e9414530f6f9e77b03380c
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-02-24 21:09:30 +0200

    updates!

--- math/advanced_linear_algebra/main.py
+++ math/advanced_linear_algebra/main.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
 if __name__ == '__main__':
-    cofactor = __import__('2-cofactor').cofactor
+    definiteness = __import__('5-definiteness').definiteness
+    import numpy as np
 
-    mat1 = [[5]]
-    mat2 = [[1, 2], [3, 4]]
-    mat3 = [[1, 1], [1, 1]]
-    mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
-    mat5 = []
-    mat6 = [[1, 2, 3], [4, 5, 6]]
+    mat1 = np.array([[5, 1], [1, 1]])
+    mat2 = np.array([[2, 4], [4, 8]])
+    mat3 = np.array([[-1, 1], [1, -1]])
+    mat4 = np.array([[-2, 4], [4, -9]])
+    mat5 = np.array([[1, 2], [2, 1]])
+    mat6 = np.array([])
+    mat7 = np.array([[1, 2, 3], [4, 5, 6]])
+    mat8 = [[1, 2], [1, 2]]
 
-    print(cofactor(mat1))
-    print(cofactor(mat2))
-    print(cofactor(mat3))
-    print(cofactor(mat4))
+    print(definiteness(mat1))
+    print(definiteness(mat2))
+    print(definiteness(mat3))
+    print(definiteness(mat4))
+    print(definiteness(mat5))
+    print(definiteness(mat6))
+    print(definiteness(mat7))
     try:
-        cofactor(mat5)
-    except Exception as e:
-        print(e)
-    try:
-        cofactor(mat6)
+        definiteness(mat8)
     except Exception as e:
         print(e)
