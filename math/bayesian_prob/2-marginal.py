37aa3aba50f4259e64f356b9453983f58c239ef0
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-03-19 17:23:15 +0000

    updates

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -18,7 +18,20 @@ def marginal(x, n, P, Pr):
     Returns:
         float: The marginal probability of obtaining x and n.
     """
-    intersection = intersection(x, n, P, Pr)
+    for value in range(P.shape[0]):
+        if P[value] > 1 or P[value] < 0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+        if Pr[value] > 1 or Pr[value] < 0:
+            raise ValueError("All values in Pr must be in the range [0, 1]")
+    if np.isclose([np.sum(Pr)], [1]) == [False]:
+        raise ValueError("Pr must sum to 1")
+
+    factorial = np.math.factorial
+    fact_coefficient = factorial(n) / (factorial(n - x) * factorial(x))
+    likelihood = fact_coefficient * (P ** x) * ((1 - P) ** (n - x))
+
+    intersection = likelihood * Pr
+
     marginal_prob = np.sum(intersection)
 
     return marginal_prob
