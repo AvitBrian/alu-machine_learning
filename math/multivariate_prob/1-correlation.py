78efcb145c9a003d8ee7bdcf75deeef3ec262217
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-03-19 14:26:14 +0000

    updates

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -5,27 +5,25 @@
 """
 import numpy as np
 
-
 def correlation(C):
     """
     Calculates a correlation matrix from a covariance matrix.
 
     Args:
-      C: A 2D numpy.ndarray of shape (d, d)
-      containing the covariance matrix.
+        C: A 2D numpy.ndarray of shape (d, d) containing the covariance matrix.
 
     Returns:
-      A numpy.ndarray of shape (d, d)
-      containing the correlation matrix.
+        A numpy.ndarray of shape (d, d) containing the correlation matrix.
+
     Raises:
-      TypeError: If C is not a numpy.ndarray.
-      ValueError: If C is not a 2D square matrix.
+        TypeError: If C is not a numpy.ndarray.
+        ValueError: If C is not a 2D square matrix.
     """
     if not isinstance(C, np.ndarray):
-      raise TypeError("C must be a numpy.ndarray")
+        raise TypeError("C must be a numpy.ndarray")
 
     if C.ndim != 2 or C.shape[0] != C.shape[1]:
-      raise ValueError("C must be a 2D square matrix")
+        raise ValueError("C must be a 2D square matrix")
 
     diagonal = np.diag(np.ones(C.shape[0]))
     std_dev = np.sqrt(np.add(C, diagonal))
