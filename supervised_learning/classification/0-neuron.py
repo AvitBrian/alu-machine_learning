24d2ed4c40f2b65ca7a1f840b7c529c2ee83638c
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-05-17 20:18:04 +0200

    update

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -9,7 +9,7 @@ import numpy as np
 class Neuron:
     '''
     Class that represents a single neuron performing binary classification.
-    
+
     Attributes:
         W (numpy.ndarray): The weights vector for the neuron. It has shape (1, nx).
         b (int): The bias for the neuron.
@@ -18,10 +18,10 @@ class Neuron:
     def __init__(self, nx):
         '''
         Initializes a Neuron instance.
-        
+
         Args:
             nx (int): The number of input features to the neuron.
-        
+
         Raises:
             TypeError: If nx is not an integer.
             ValueError: If nx is not a positive integer.
@@ -30,7 +30,7 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        
+
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
