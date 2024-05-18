f2574054c991b720503af2165b43ac1d3dc8daff
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-05-18 15:38:01 +0200

    updates

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -22,38 +22,38 @@ class DeepNeuralNetwork:
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
-        self.nx = nx
-        self.layers = layers
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        self.__nx = nx
+        self.__layers = layers
 
-        for i in range(self.L):
+        for i in range(self.__L):
             if not isinstance(layers[i], int) or layers[i] < 1:
                 raise TypeError("layers must be a list of positive integers")
             if i == 0:
-                self.weights["W1"] = (
+                self.__weights["W1"] = (
                     np.random.randn(layers[i], nx) * np.sqrt(2 / nx))
             else:
-                self.weights["W" + str(i + 1)] = np.random.randn(
+                self.__weights["W" + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]
                 ) * np.sqrt(2 / layers[i - 1])
-            self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
+            self.__weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
 
-            @property
-            def L(self):
-                """Getter method for the L attribute."""
-                return self.__L
+    @property
+    def L(self):
+        """Getter method for the L attribute."""
+        return self.__L
 
-            @property
-            def cache(self):
-                """Getter method for the cache attribute."""
-                return self.__cache
+    @property
+    def cache(self):
+        """Getter method for the cache attribute."""
+        return self.__cache
 
-            @property
-            def weights(self):
-                """Getter method for the weights attribute."""
-                return self.__weights
+    @property
+    def weights(self):
+        """Getter method for the weights attribute."""
+        return self.__weights
 
     def forward_prop(self, X):
         '''Calculates the forward propagation'''
