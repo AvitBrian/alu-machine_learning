c83290bb9c2dbcd2c0bfd7f9dc4ec60a8a0ce16e
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-05-18 15:31:57 +0200

    updates

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """
-    This class represents a single neuron performing
-    binary classification.
+This class represents a single neuron performing
+binary classification.
 """
 import numpy as np
 
 
 class DeepNeuralNetwork:
     """
-        class: DeepNeuralNetwork
+    class: DeepNeuralNetwork
     """
 
     def __init__(self, nx, layers):
         """
-            Initializes the deep neural network object.
+        Initializes the deep neural network object.
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -22,41 +22,41 @@ class DeepNeuralNetwork:
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
         self.nx = nx
         self.layers = layers
 
-        for i in range(self.L):
+        for i in range(self.__L):
             if not isinstance(layers[i], int) or layers[i] < 1:
                 raise TypeError("layers must be a list of positive integers")
             if i == 0:
-                self.weights["W1"] = (
+                self.__weights["W1"] = (
                     np.random.randn(layers[i], nx) * np.sqrt(2 / nx))
             else:
-                self.weights["W" + str(i + 1)] = np.random.randn(
+                self.__weights["W" + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]
                 ) * np.sqrt(2 / layers[i - 1])
-            self.weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
+            self.__weights["b" + str(i + 1)] = np.zeros((layers[i], 1))
 
-        @property
-        def L(self):
-            """Getter method for the L attribute."""
-            return self.__L
+    @property
+    def L(self):
+        """Getter method for the L attribute."""
+        return self.__L
 
-        @property
-        def cache(self):
-            """Getter method for the cache attribute."""
-            return self.__cache
+    @property
+    def cache(self):
+        """Getter method for the cache attribute."""
+        return self.__cache
 
-        @property
-        def weights(self):
-            """Getter method for the weights attribute."""
-            return self.__weights
+    @property
+    def weights(self):
+        """Getter method for the weights attribute."""
+        return self.__weights
 
     def forward_prop(self, X):
-        '''Calculates the forward propagation'''
+        """Calculates the forward propagation"""
         self.__cache["A0"] = X
         for i in range(self.__L):
             W = self.__weights["W{}".format(i + 1)]
