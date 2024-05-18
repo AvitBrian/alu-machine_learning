f84f08fd1fb5e6cbf875615ac8f9d98381c4dbc8
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-05-18 20:52:49 +0200

    updates

--- supervised_learning/classification/28-deep_neural_network.py
+++ supervised_learning/classification/28-deep_neural_network.py
@@ -12,9 +12,9 @@ class DeepNeuralNetwork:
     """
     Class representing a single neuron performing binary classification.
     """
-
     def __init__(self, nx, layers, activation='sig'):
-        """DeepNeuralNetwork class constructor."""
+        """ Instantiation function
+        """
         if activation not in ['sig', 'tanh']:
             raise ValueError("activation must be 'sig' or 'tanh'")
         if not isinstance(nx, int):
@@ -42,80 +42,93 @@ class DeepNeuralNetwork:
             else:
                 self.__weights['W' + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
             self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
 
     @property
     def activation(self):
-        """Return activation function."""
+        """ Return activation function """
         return self.__activation
 
     @property
+    def L(self):
+        """ Return layers in the neural network"""
+        return self.__L
+
+    @property
     def cache(self):
-        """Return the cache attribute."""
+        """ Return dictionary with intermediate values of the network"""
         return self.__cache
 
     @property
     def weights(self):
-        """Return the weights attribute."""
+        """Return weights and bias dictionary"""
         return self.__weights
 
     def forward_prop(self, X):
-        """Forward propagation."""
-        self.__cache["A0"] = X
-        for i in range(1, self.__L + 1):
-            W = self.__weights['W' + str(i)]
-            b = self.__weights['b' + str(i)]
-            A = self.__cache['A' + str(i - 1)]
+        """ Forward propagation """
+        self.cache["A0"] = X
+        for i in range(1, self.L+1):
+            W = self.weights['W'+str(i)]
+            b = self.weights['b'+str(i)]
+            A = self.cache['A'+str(i - 1)]
             z = np.matmul(W, A) + b
-            if i != self.__L:
-                if self.__activation == 'sig':
-                    A = 1 / (1 + np.exp(-z))
-                elif self.__activation == 'tanh':
-                    A = np.tanh(z)
+            if i != self.L:
+                if self.activation == 'sig':
+                    A = 1 / (1 + np.exp(-z))  # sigmoid function
+                elif self.activation == 'tanh':
+                    A = np.tanh(z)  # tanh function
             else:
-                A = np.exp(z) / np.sum(np.exp(z), axis=0)
-            self.__cache["A" + str(i)] = A
-        return self.__cache["A" + str(i)], self.__cache
+                A = np.exp(z) / np.sum(np.exp(z), axis=0)  # softmax function
+            self.cache["A"+str(i)] = A
+        return self.cache["A"+str(i)], self.cache
 
     def cost(self, Y, A):
-        """Calculate the cost of the Neural Network."""
+        """ Calculate the cost of the Neural Network \
+            using categorical cross-entropy.
+        """
         cost = -np.sum(Y * np.log(A)) / Y.shape[1]
         return cost
 
     def evaluate(self, X, Y):
-        """Evaluate the neural network."""
+        """ Evaluate the neural network
+        """
         self.forward_prop(X)
-        A = self.__cache.get("A" + str(self.__L))
+        A = self.cache.get("A" + str(self.L))
         prediction = np.eye(A.shape[0])[np.argmax(A, axis=0)].T
         cost = self.cost(Y, A)
         return prediction, cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
-        """Calculate one pass of gradient descent on the neural network."""
+        """ Calculate one pass of gradient descent on the neural network
+        """
         m = Y.shape[1]
 
-        for i in range(self.__L, 0, -1):
+        for i in range(self.L, 0, -1):
+
             A_prev = cache["A" + str(i - 1)]
             A = cache["A" + str(i)]
-            W = self.__weights["W" + str(i)]
+            W = self.weights["W" + str(i)]
 
-            if i == self.__L:
+            if i == self.L:
                 dz = A - Y
             else:
-                if self.__activation == 'sig':
-                    dz = da * (A * (1 - A))
-                elif self.__activation == 'tanh':
-                    dz = da * (1 - A ** 2)
+                if self.activation == 'sig':
+                    dz = da * (A * (1 - A))  # sigmoid derivative
+                elif self.activation == 'tanh':
+                    dz = da * (1 - A**2)  # tanh derivative
 
             db = dz.mean(axis=1, keepdims=True)
             dw = np.matmul(dz, A_prev.T) / m
             da = np.matmul(W.T, dz)
-            self.__weights['W' + str(i)] -= (alpha * dw)
-            self.__weights['b' + str(i)] -= (alpha * db)
+            self.weights['W' + str(i)] -= (alpha * dw)
+            self.weights['b' + str(i)] -= (alpha * db)
 
     def train(self, X, Y, iterations=5000,
               alpha=0.05, verbose=True, graph=True, step=100):
-        """Train the deep neural network."""
+        """ Train the deep neural network
+        """
+
         if not isinstance(iterations, int):
             raise TypeError('iterations must be an integer')
         if iterations < 1:
@@ -128,9 +141,10 @@ class DeepNeuralNetwork:
         costs = []
         for i in range(iterations):
             self.forward_prop(X)
-            self.gradient_descent(Y, self.__cache, alpha)
+            self.gradient_descent(Y, self.cache, alpha)
             if verbose and i % step == 0:
-                cost = self.cost(Y, self.__cache["A" + str(self.__L)])
+
+                cost = self.cost(Y, self.cache["A"+str(self.L)])
                 costs.append(cost)
                 print('Cost after {} iterations: {}'.format(i, cost))
         if graph:
@@ -142,7 +156,8 @@ class DeepNeuralNetwork:
         return self.evaluate(X, Y)
 
     def save(self, filename):
-        """Save the instance object to a file in pickle format."""
+        """ Save the instance object to a file in pickle format
+        """
         if not filename.endswith(".pkl"):
             filename += ".pkl"
         with open(filename, 'wb') as f:
@@ -150,7 +165,8 @@ class DeepNeuralNetwork:
 
     @staticmethod
     def load(filename):
-        """Load a pickled DeepNeuralNetwork object."""
+        """ Load a pickled DeepNeuralNetwork object
+        """
         try:
             with open(filename, 'rb') as f:
                 return pickle.load(f)
