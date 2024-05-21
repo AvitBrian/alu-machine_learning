ffd3390bf923d0b718c035ae0774118f98d91dfd
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-05-21 17:24:09 +0200

    updates

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 """
-    This module performs forward propagation.
+    This module performs forward propagation,
+    without importing any modules.
 """
 import tensorflow as tf
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
-    function: forward_prop
-    performs forward propagation
-    @x: is the input data
-    @layer_sizes: is a list containing the number of nodes in each layer
-    of the network
-    @activations: is a list containing the activation functions for each
-    layer of the network
-    Return: the prediction of the network in tensor form
+    x is the placeholder for the input data
+    layer_sizes is a list containing the number of nodes in each layer of the network
+    activations is a list containing the activation functions for each layer of the network
+    Returns: the prediction of the network in tensor form
     """
     for i in range(len(layer_sizes)):
-        init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-        layer = tf.layers.Dense(layer_sizes[i], activations[i],
-                                kernel_initializer=init, name='layer')
-        x = layer(x)
-    return x
+        if i == 0:
+            layer = x
+        layer = create_layer(layer, layer_sizes[i], activations[i])
+    return layer
