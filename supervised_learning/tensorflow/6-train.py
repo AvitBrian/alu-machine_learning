3c50b96e622bcca6a33e522c7897c303bde5a225
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-05-23 09:58:13 +0200

    updates 6-task

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -15,31 +15,14 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     function: train
     trains a neural network
     """
-    tf.reset_default_graph()
-
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    tf.add_to_collection('placeholders', x)
-    tf.add_to_collection('placeholders', y)
-
     y_pred = forward_prop(x, layer_sizes, activations)
-    tf.add_to_collection('tensors', y_pred)
-
     loss = calculate_loss(y, y_pred)
-    tf.add_to_collection('tensors', loss)
-
-    train_op = create_train_op(loss, alpha)
-    tf.add_to_collection('operation', train_op)
-
     accuracy = calculate_accuracy(y, y_pred)
-    tf.add_to_collection('tensors', accuracy)
-
-    init = tf.global_variables_initializer()
-
+    train_op = create_train_op(loss, alpha)
     saver = tf.train.Saver()
-
     with tf.Session() as sess:
         sess.run(init)
-
         for i in range(iterations + 1):
             _, train_cost = sess.run([train_op, loss], feed_dict={x: X_train, y: Y_train})
             train_accuracy = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
@@ -54,6 +37,5 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
                 print(f"\tValidation Accuracy: {valid_accuracy}")
 
         save_path = saver.save(sess, save_path)
-        print("Model saved in path: %s" % save_path)
-
-    return save_path
+        
+        return save_path
