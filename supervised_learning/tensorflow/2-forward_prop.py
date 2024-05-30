10d6909805819e10b3a88164d6d9a27df8ce2a96
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-05-30 17:58:58 +0200

    final updates

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -3,19 +3,16 @@
     This module performs forward propagation,
     without importing any modules.
 """
-import tensorflow as tf 
+
+
+import tensorflow as tf
 create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
-    """
-    x is the placeholder for the input data
-    layer_sizes is a list containing the number of nodes in each layer of the network
-    activations is a list containing the activation functions for each layer of the network
-    Returns: the prediction of the network in tensor form
-    """
-    prediction = x
+    """creates the forward propagation"""
+    L = x
     for i in range(len(layer_sizes)):
-        prediction = create_layer(prediction,
-                                  layer_sizes[i], activations[i])
-    return prediction
+        L = create_layer(L, layer_sizes[i],
+                         activations[i])
+    return L
