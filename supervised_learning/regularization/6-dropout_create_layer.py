4fa1c4301700a6da4b5460a346306f74c1e27723
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-01 01:28:43 +0200

    updates

---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    This module performs l2 regularization.
+"""
+import tensorflow as tf
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """Creates a layer of a neural network using dropout."""
+    dropout = tf.layers.Dropout(rate=1 - keep_prob)
+    return dropout(activation(tf.layers.dense(prev, units=n)))
