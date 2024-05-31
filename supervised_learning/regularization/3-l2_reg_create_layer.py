6c9916b9303694ed9b5bcb2ee3ed75bc16c3a281
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-01 01:10:02 +0200

    updates

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,5 +7,9 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """ Creates a tensorflow layer that includes L2 regularization. """
-    return tf.layers.dense(prev, units=n, activation=activation,
-                           kernel_regularizer=tf.contrib.layers.l2_regularizer(lambtha))
+    ker = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    reg = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=ker,
+                            kernel_regularizer=reg)
+    return layer(prev)
