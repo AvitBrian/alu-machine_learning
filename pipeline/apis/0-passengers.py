54b761c95e5a353a6bf9ae4532676c8b35b28624
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-28 18:02:30 +0200

    initial commit

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -16,14 +16,13 @@ def availableShips(passengerCount):
     if response.status_code == 200:
         data = response.json()
         for each_ship in data["results"]:
-            try:
-                passengers = int(each_ship['passengers'].replace(',', ''))
-                if passengers >= passengerCount:
-                    ships.append(each_ship['name'])
-                else:
-                    pass
-            except Exception as e:
-                pass
+                if (each_ship['passengers'] != "n/a"
+                    and each_ship['passengers'] != "unknown"
+                    and each_ship['passengers'] != "none"
+                    and each_ship['passengers'] != "0"):
+                    passengers = int(each_ship['passengers'].replace(',', ''))
+                    if passengers >= passengerCount:
+                        ships.append(each_ship['name'])
 
     url = data["next"]
     return ships
