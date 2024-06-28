9898102245ee83ff642f376a7417d0a959a76718
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-28 18:16:23 +0200

    initial commit

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -11,20 +11,21 @@ def availableShips(passengerCount):
     Returns the list of ships.
     """
     url = "https://swapi-api.alx-tools.com/api/starships/"
-    ships = []
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        for each_ship in data["results"]:
-            passengers = each_ship['passengers'].replace(',', '')
-            if all([
-                passengers != "n/a",
-                passengers != "unknown",
-                passengers != "none",
-                passengers != "0",
-            ]):
-                if int(passengers) >= passengerCount:
-                    ships.append(each_ship['name'])
+    ships = []        
+    while url is not None:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            for each_ship in data["results"]:
+                passengers = each_ship['passengers'].replace(',', '')
+                if all([
+                    passengers != "n/a",
+                    passengers != "unknown",
+                    passengers != "none",
+                    passengers != "0",
+                ]):
+                    if int(passengers) >= passengerCount:
+                        ships.append(each_ship['name'])
 
-    url = data["next"]
+        url = data["next"]
     return ships
