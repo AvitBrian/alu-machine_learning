3f1d8897e29145978ddcaa66b2898c5689ddc8cb
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-28 19:02:31 +0200

    update

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -11,21 +11,20 @@ def sentientPlanets():
     Returns a list of planet names.
     """
     url = "https://swapi-api.alx-tools.com/api/species/"
-    planets = []
-    classisfication = "sentient"
-    while url is not None:
+    sentient_planets = []
+    while url:
         response = requests.get(url)
         data = response.json()
-        for species in data["results"]:
-            if all([
-                species["classification"].lower() == classisfication,
-                species["homeworld"]]
-            ):
-                homeworld = species["homeworld"]
-                res_homeworld = requests.get(homeworld)
-                data_homeword = res_homeworld.json()
-                planets.append(data_homeword["name"])
+        if response.status_code == 200:
+            for each_species in data["results"]:
+                species_class = each_species["classification"].lower()
+                if all([species_class == "sentient",
+                    each_species["homeworld"] != None]):
+                    homeworld_url = each_species["homeworld"]
+                    homeworld_response = requests.get(homeworld_url)
+                    homeworld_data = homeworld_response.json()
+                    sentient_planets.append(homeworld_data["name"])
 
         url = data["next"]
-    return planets
 
+    return sentient_planets
