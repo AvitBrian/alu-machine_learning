bd71f3ad310e01da5be91a6982d1ba83836c293c
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-28 18:04:21 +0200

    initial commit

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    This module returns he list of ships
+    This module returns the list of ships
     that can hold a given number of passengers
 """
 import requests
@@ -16,13 +16,15 @@ def availableShips(passengerCount):
     if response.status_code == 200:
         data = response.json()
         for each_ship in data["results"]:
-                if (each_ship['passengers'] != "n/a"
-                    and each_ship['passengers'] != "unknown"
-                    and each_ship['passengers'] != "none"
-                    and each_ship['passengers'] != "0"):
-                    passengers = int(each_ship['passengers'].replace(',', ''))
-                    if passengers >= passengerCount:
-                        ships.append(each_ship['name'])
+            passengers = each_ship['passengers'].replace(',', '')
+            if all([
+                passengers != "n/a",
+                passengers != "unknown",
+                passengers != "none",
+                passengers != "0",
+                int(passengers) >= passengerCount
+            ]):
+                ships.append(each_ship['name'])
 
     url = data["next"]
     return ships
