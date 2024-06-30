4bf1c076dec356a8ac3780e56ab45530ba7b8e5e
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-06-30 21:12:06 +0000

    finally fixed the permission errors!

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -19,9 +19,12 @@ if __name__ == '__main__':
         if response.status_code == 200:
             user_data = response.json()
             print(user_data.get('location', 'Location not specified'))
+        elif response.status_code == 403:
+            reset_time = int(response.headers.get('X-RateLimit-Reset', 0))
+            reset_in = (reset_time - time.time()) / 60
+            print('Reset in {} min'.format(int(reset_in)))
         elif response.status_code == 404:
             print('Not found')
-
         else:
             print('Error: {}'.format(response.status_code))
     except requests.exceptions.RequestException as e:
