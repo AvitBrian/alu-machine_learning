173f03579662821e6d6c780584a222b424ad5b45
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-30 23:17:18 +0200

    updates

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -6,6 +6,7 @@
 import requests
 import sys
 from datetime import datetime, timedelta
+from datetime import datetime, timedelta
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -20,8 +21,7 @@ if __name__ == '__main__':
             user_data = response.json()
             print(user_data.get('location', 'Location not specified'))
         elif response.status_code == 403:
-            time_to_reset = int(response.headers.get('X-RateLimit-Reset', 0))
-            reset_time = datetime.fromtimestamp(time_to_reset)
+            reset_time = datetime.fromtimestamp(int(response.headers.get('X-RateLimit-Reset', 0)))
             now = datetime.now()
             minutes_remaining = (reset_time - now).total_seconds() // 60
             print('Reset in {} min'.format(int(minutes_remaining)))
