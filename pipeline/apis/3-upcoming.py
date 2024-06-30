502dda3c8799b081c9c31422e89282d10d2f2909
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-06-30 21:30:55 +0000

    final updates

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -5,6 +5,7 @@ This module retrieves information about the upcoming SpaceX launch.
 import requests
 from datetime import datetime, timedelta, timezone
 
+
 def get_launch_info(launch_data):
     """
     Retrieves and formats the launch information.
@@ -19,11 +20,11 @@ def get_launch_info(launch_data):
     launch_date_str = launch_date_local.strftime('%Y-%m-%dT%H:%M:%S%z')
     launch_date_str = "{}:{}".format(launch_date_str[:-2], launch_date_str[-2:])
 
-    rocket_url = f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
+    rocket_url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
     rocket_response = requests.get(rocket_url)
     rocket_name = rocket_response.json()["name"]
 
-    launchpad_url = f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}"
+    launchpad_url = "https://api.spacexdata.com/v4/launchpads/{}".format(launchpad_id)
     launchpad_response = requests.get(launchpad_url)
     launchpad_data = launchpad_response.json()
     launchpad_name = launchpad_data["name"]
