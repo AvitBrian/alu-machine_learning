35d52c3eb227cc315cb6503ae18d941a2f69ec16
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-06-30 19:30:18 +0200

    updates

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
-'''
-    This module returns the location of a user
-    with a github api url.
-'''
 import requests
 import sys
-import time
-
-
-def get_user_location(api_url):
-    '''Returns the user's location'''
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        user_data = response.json()
-        print(user_data.get('location', 'Location not specified'))
-    elif response.status_code == 403:
-        reset_time = int(response.headers.get('X-RateLimit-Reset', 0))
-        reset= (reset_time - time.time()) / 60
-        print('Reset in {} min'.format(reset))
-    elif response.status_code == 404:
-        print('Not found')
-    else:
-        print('Error: {}'.format(response.status_code))
-
+from datetime import datetime, timedelta
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Usage: ./2-user_location.py <API_URL>')
-    else: 
-        get_user_location(sys.argv[1])
+        print("Usage: ./2-user_location.py <API_URL>")
+        sys.exit(1)
+
+    api_url = sys.argv[1]
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        if response.status_code == 404:
+            print("Not found")
+        elif response.status_code == 403:
+            reset_time = datetime.fromtimestamp(int(response.headers['X-RateLimit-Reset']))
+            now = datetime.now()
+            minutes_remaining = (reset_time - now).total_seconds() // 60
+            print(f"Reset in {int(minutes_remaining)} min")
+        else:
+            print(e)
+        sys.exit(1)
+
+    user_data = response.json()
+    location = user_data.get('location', 'Not available')
+    print(location)
