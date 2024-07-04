746a366a48dcca1f9b11f809751b18ada74c9f98
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-07-05 00:42:03 +0200

    updates

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,4 +1,6 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
+USE hbtn_0c_0
+source temperatures.sql
 SELECT city, AVG("value") AS avg_temp
 FROM table_name
 GROUP BY city
