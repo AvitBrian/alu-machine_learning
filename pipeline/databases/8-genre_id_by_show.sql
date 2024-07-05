1585873f802dbbfb95a0de4d54b4ed4d1c964c36
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-07-05 20:38:38 +0200

    UPDATES

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- lists tv shows and there genre ids
+-- lists tv shows and their genre ids
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
