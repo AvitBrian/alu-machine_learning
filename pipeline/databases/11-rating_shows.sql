162463784fbd69db00b77395dcd30319569bc79c
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-07-05 20:43:08 +0200

    UPDATES

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,6 +1,6 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT tv_shows.title AS title,
-  IFNULL(MAX(tv_show_ratings.rate), 0) AS rating
+  IFNULL(SUM(tv_show_ratings.rate), 0) AS rating
 FROM tv_shows
   LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
