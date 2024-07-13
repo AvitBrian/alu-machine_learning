0dd3144681ccae2db81a58e7a0dbc286980dc350
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-07-13 19:54:54 +0200

    updates

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,9 +1,9 @@
 -- Creates a trigger that decreases the quantity of an item after an order is made
 DROP TRIGGER IF EXISTS decrease_quantity;
-CREATE TRIGGER decrease_quantity
+DELIMITER $$ CREATE TRIGGER decrease_quantity
 AFTER
-INSERT ON orders FOR EACH ROW BEGIN
+INSERT ON `orders` FOR EACH ROW BEGIN
 UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = NEW.item_name;
-END;
+SET quantity = quantity - new.number
+WHERE items.name = new.item_name;
+END $$ DELIMITER;
