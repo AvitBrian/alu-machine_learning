7ad0299cce5a557e56d1a9ddf370dba80b76185d
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-07-13 19:45:56 +0200

    updates

--- pipeline/databases/19-bonus.sql
+++ pipeline/databases/19-bonus.sql
@@ -1,4 +1,4 @@
--- Creates a stored procedure AddBonus that adds a new correction for a student
+-- Creates a new stored procedure that adds a bonus to a user
 DELIMITER // CREATE PROCEDURE AddBonus (
 	IN user_id INTEGER,
 	IN project_name VARCHAR(255),
--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,4 +1,4 @@
--- create a stored procedure
+-- creates a stored procedure ComputeAverageScoreForUser that computes the average score of a user and updates the average_score field in the users table.
 DELIMITER // CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_new INTEGER) BEGIN
 UPDATE users
 SET average_score =(
