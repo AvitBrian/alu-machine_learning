41a04c40a17c4f2b9f379b4599feb2d290590062
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-07-13 21:04:29 +0200

    updates

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -1,7 +1,7 @@
 -- Creates a trigger that resets the validation of an email after it is updated
 DROP TRIGGER IF EXISTS reset_validation;
 DELIMITER $$ CREATE TRIGGER reset_validation BEFORE
-UPDATE ON users FOR EACH ROW BEGIN IF NEW.email <> OLD.email THEN
-SET NEW.valid_email = 0;
+UPDATE ON `users` FOR EACH ROW BEGIN IF STRCMP(old.email, new.email) <> 0 THEN
+SET new.valid_email = 0;
 END IF;
 END $$ DELIMITER;
