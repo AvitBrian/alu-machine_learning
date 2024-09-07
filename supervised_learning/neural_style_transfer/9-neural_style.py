e5a57a51d3acf130b8e1bab5c492dde816eeec8b
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-09-07 02:18:46 +0200

    Updates.

--- supervised_learning/neural_style_transfer/9-neural_style.py
+++ supervised_learning/neural_style_transfer/9-neural_style.py
@@ -23,7 +23,7 @@ class NST:
         Initializes the NST class with style and content images,
         and the respective weights for content and style cost
         """
-        tf.enable_eager_execution()
+        tf.compat.v1.enable_eager_execution()
 
         # Validate inputs
         if not isinstance(style_image, np.ndarray) or \
@@ -78,8 +78,10 @@ class NST:
             w_new = 512
             h_new = int(h * (512 / w))
 
-        resized = tf.image.resize_bicubic(np.expand_dims(image, axis=0),
-                                          size=(h_new, w_new))
+        resized = tf.image.resize(np.expand_dims(image, axis=0),
+                                          size=(h_new, w_new),
+                                          method="bicubic"
+                  )
         rescaled = resized / 255
         rescaled = tf.clip_by_value(rescaled, 0, 1)
         return rescaled
@@ -203,8 +205,7 @@ class NST:
                 )
             )
 
-        content_output = tf.cast(content_output, tf.float64)
-        content_feature = tf.cast(self.content_feature, tf.float64)
+        content_feature = self.content_feature
 
         content_cost = tf.reduce_mean(tf.square(
             content_output - content_feature
@@ -225,18 +226,17 @@ class NST:
                 )
             )
 
-        J_content = self.content_cost(generated_image)
+        J_content = self.content_cost(self.model(generated_image)[-1])
         J_style = self.style_cost(self.model(generated_image)[:-1])
-        self.alpha = 1e3
-        self.beta = 1e-2
         J = self.alpha * J_content + self.beta * J_style
 
         return J, J_content, J_style
 
-    def compute_grade(self, generated_image):
+    def compute_grads(self, generated_image):
         """
         Calculates the gradients for the tf.tensor generated_image
         """
+        generated_image = tf.convert_to_tensor(generated_image)
         if not isinstance(generated_image, tf.Tensor) or\
             generated_image.shape != self.content_image.shape:
                 raise TypeError(
@@ -299,13 +299,25 @@ class NST:
             
             optimizer.apply_gradients([(gradients, generated_image)])
             
-            generated_image.assign(tf.clip_by_value(generated_image, clip_value_min=0.0, clip_value_max=1.0))
+            generated_image.assign(
+                tf.clip_by_value(
+                    generated_image, clip_value_min=0.0, clip_value_max=1.0
+                )
+            )
             
             if J_total < best_cost:
                 best_cost = J_total
                 best_image = generated_image.numpy()
             
             if step is not None and i % step == 0:
-                print(f"Cost at iteration {i}: {J_total.numpy()}, content {J_content.numpy()}, style {J_style.numpy()}")
+                J_total_np = J_total.numpy()
+                J_content_np = J_content.numpy()
+                J_style_np = J_style.numpy()
+                
+                print(
+                    f"Cost at iteration {i}: {J_total_np}, "
+                    f"content {J_content_np}, "
+                    f"style {J_style_np}"
+                )
         
         return best_image, best_cost
