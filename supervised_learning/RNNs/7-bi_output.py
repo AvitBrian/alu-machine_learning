a13a0a9e488f714ee449db4b5bd32ad1a812c8b1
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-09-14 23:07:59 +0200

    updates

--- supervised_learning/RNNs/7-bi_output.py
+++ supervised_learning/RNNs/7-bi_output.py
@@ -40,12 +40,11 @@ class BidirectionalCell:
         '''
         Method that calculates the output of the cell
         '''
-        t, m, h = H.shape
+        m, h = H.shape
+        
+        Y = np.zeros((m, self.Wy.shape[1]))
 
-        Y = []
+        exp_Y = np.exp(np.dot(H, self.Wy) + self.by)
+        Y = exp_Y / np.sum(exp_Y, axis=1, keepdims=True)
 
-        for step in range(t):
-            y = self.softmax(np.matmul(H[step], self.Wy) + self.by)
-            Y.append(y)
-
-        return np.array(Y)
+        return Y
