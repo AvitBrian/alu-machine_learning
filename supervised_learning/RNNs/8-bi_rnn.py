6e5dc1ec5435d3b8a76e3d0399e1d0c551f91c07
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-09-14 22:28:14 +0200

    updates

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -7,7 +7,7 @@ Bidirectional Cell Forward
 import numpy as np
 
 
-def bi_rnn(bi_cell, X, h_0, h_t):
+def bi_rnn(bi_cell, X, h_0, h_T):
     '''
     Function that performs forward propagation for a bidirectional RNN
     '''
