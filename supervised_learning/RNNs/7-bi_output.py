a41f8b5f73d7de76ffd1aaba6754c51fe7ff3a77
Avit Brian MUGISHA <113444617+AvitBrian@users.noreply.github.com>
2024-09-14 22:24:10 +0200

    updates

--- supervised_learning/RNNs/7-bi_output.py
+++ supervised_learning/RNNs/7-bi_output.py
@@ -13,7 +13,7 @@ class BidirectionalCell:
     '''
     def __init__(self, i, h, o):
         '''
-        Class constructor
+        defines the constructor
         '''
         self.Whf = np.random.normal(size=(h + i, h))
         self.Whb = np.random.normal(size=(h + i, h))
@@ -23,25 +23,29 @@ class BidirectionalCell:
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        ''' Method that performs forward propagation for one time step '''
+        '''
+        performs forward propagation for one time step
+        '''
         h_x = np.hstack((h_prev, x_t))
         h_next = np.tanh(np.dot(h_x, self.Whf) + self.bhf)
         return h_next
 
     def backward(self, h_prev, x_t):
-        ''' Method that performs backward propagation for one time step '''
-        h_x = np.hstack((h_prev, x_t))  # Use h_prev and x_t, not h_next
+        '''
+        performs backward propagation for one time step
+        '''
+        h_x = np.hstack((h_prev, x_t))
         h_next = np.tanh(np.dot(h_x, self.Whb) + self.bhb)
         return h_next
 
-
     def output(self, H):
-        ''' Method that calculates the output of the cell '''
+        '''
+        Method that calculates the output of the cell
+        '''
         t, m, h = H.shape
         Y = np.zeros((t, m, self.Wy.shape[1]))
 
         for i in range(t):
-            # No need to re-stack H, use it directly
             Y[i] = np.dot(H[i], self.Wy) + self.by
             exp_Y = np.exp(Y[i])
             Y[i] = exp_Y / np.sum(exp_Y, axis=1, keepdims=True)
