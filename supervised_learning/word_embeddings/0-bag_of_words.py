2072e0d180e66262a6f87a65acdcabaf30f44265
Avit Brian <b.mugisha2@alustudent.com>
2024-09-29 16:27:57 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -10,17 +10,9 @@ def bag_of_words(sentences, vocab=None):
     '''
     creates a bag of words embedding matrix
     '''
-    if vocab is None:
         vocab = set()
-    for sentence in sentences:
-        words = sentence.split()
-        for word in words:
-            vocab.add(word)
-    vocab = sorted(list(vocab))
-    embeddings = np.zeros((len(sentences), len(vocab)))
-    for i, sentence in enumerate(sentences):
-        words = sentence.split()
-        for j, word in enumerate(vocab):
-            embeddings[i, j] = words.count(word)
-
-    return embeddings, vocab
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X_train_counts = vectorizer.fit_transform(sentences)
+    embeddings = X_train_counts.toarray()
+    features = vectorizer.get_feature_names()
+    return embeddings, features
