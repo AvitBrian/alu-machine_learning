27b5c98f0683f87f64bbecf73bab748559fc9d76
Avit Brian <b.mugisha2@alustudent.com>
2024-09-29 16:31:39 +0200

    updates

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """
 this module contains the function bag_of_words
+it creates a word embedding matrix using the bag of words
+method
 """
-
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
     '''
     creates a bag of words embedding matrix
     '''
-        vocab = set()
     vectorizer = CountVectorizer(vocabulary=vocab)
     X_train_counts = vectorizer.fit_transform(sentences)
     embeddings = X_train_counts.toarray()
