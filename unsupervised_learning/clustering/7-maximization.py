66832a89cdb3b604d7c7772057ca4b448d418b19
Avit Brian <b.mugisha2@alustudent.com>
2024-10-19 00:06:08 +0200

    updates

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -5,6 +5,7 @@ Module for calculating the maximization step in the EM algorithm for a GMM
 
 import numpy as np
 
+
 def maximization(X, g):
     """
     Calculates the maximization step in the EM algorithm for a GMM
@@ -18,6 +19,11 @@ def maximization(X, g):
 
     n, d = X.shape
     k = g.shape[0]
+
+    if (np.any(g < 0) or np.any(g > 1) or 
+        not np.allclose(np.sum(g, axis=0), 1)):
+        return None, None, None
+
     pi = np.sum(g, axis=1) / n
     m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
 
