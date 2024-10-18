8d07dbad636a08abfc283343e9e60c019729ed39
Avit Brian <b.mugisha2@alustudent.com>
2024-10-18 17:46:55 +0200

    updates

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -4,7 +4,23 @@ This module implements K-means clustering algorithm
 '''
 import numpy as np
 
-initialize = __import__('0-initialize').initialize
+
+def initialize(X, k):
+    '''
+    Initializes cluster centroids for K-means
+    '''
+
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(k) is not int or k <= 0:
+        return None
+
+    n, d = X.shape
+    minimum = np.min(X, axis=0)
+    maximum = np.max(X, axis=0)
+    centroid = np.random.uniform(minimum, maximum, size=(k, d))
+
+    return centroid
 
 
 def kmeans(X, k, iterations=1000):
@@ -19,8 +35,6 @@ def kmeans(X, k, iterations=1000):
         return None, None
 
     C = initialize(X, k)
-    if C is None:
-        return None, None
 
     for _ in range(iterations):
         distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
