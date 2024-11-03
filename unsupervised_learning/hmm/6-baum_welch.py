29f1fc74d802c85c32f80c0c3aa9175f8dc94208
Avit Brian <b.mugisha2@alustudent.com>
2024-11-03 19:36:39 +0200

    updates

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-'''Performing the Baum-Welch algorithm for a hidden markov model'''
+'''Performing the Baum-Welch algorithm for a hidden Markov model'''
 
 import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
-    """
-    Performs the forward algorithm for a hidden Markov model.
-    """
+    '''Performs the forward algorithm for a hidden markov model'''
     if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
         return None, None
     T = Observation.shape[0]
@@ -28,14 +26,14 @@ def forward(Observation, Emission, Transition, Initial):
     F[:, 0] = Initial.T * Emission[:, Observation[0]]
     for i in range(1, T):
         F[:, i] = np.sum(
-            F[:, i - 1] * Transition.T * Emission[np.newaxis, :, Observation[i]].T, axis=1)
+            F[:, i - 1] * Transition.T *
+            Emission[np.newaxis, :, Observation[i]].T, axis=1)
     P = np.sum(F[:, -1])
     return P, F
 
+
 def backward(Observation, Emission, Transition, Initial):
-    """
-    Performs the backward algorithm for a hidden Markov model
-    """
+    '''Performs the backward algorithm for a hidden markov model'''
     if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
         return None, None
     T = Observation.shape[0]
@@ -60,10 +58,9 @@ def backward(Observation, Emission, Transition, Initial):
     P = np.sum(Initial.T * Emission[:, Observation[0]] * B[:, 0])
     return P, B
 
+
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
-    """
-    Function that performs the Baum-Welch algorithm for a hidden Markov model
-    """
+    '''Function that performs the Baum-Welch algorithm for a hidden Markov model'''
     N = Transition.shape[0]
     M = Emission.shape[1]
     T = Observations.shape[0]
@@ -74,18 +71,18 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
 
         xi = np.zeros((N, N, T - 1))
         for t in range(T - 1):
-            denominator = np.dot(np.dot(F[:, t].T, Transition) *
-                                 Emission[:, Observations[t + 1]] *
-                                 B[:, t + 1].T, 1)
+            denominator = (np.dot(np.dot(F[:, t].T, Transition) *
+                                  Emission[:, Observations[t + 1]] *
+                                  B[:, t + 1].T, 1))
             for i in range(N):
-                numerator = F[i, t] * Transition[i] * \
-                    Emission[:, Observations[t + 1]] * B[:, t + 1].T
+                numerator = (F[i, t] * Transition[i] *
+                             Emission[:, Observations[t + 1]] *
+                             B[:, t + 1].T)
                 xi[i, :, t] = numerator / denominator
 
         gamma = np.sum(xi, axis=1)
-        Transition = np.sum(xi, 2) / np.sum(gamma, axis=1).reshape((-1, 1))
+        Transition = np.sum(xi, axis=2) / np.sum(gamma, axis=1).reshape((-1, 1))
 
-        # Add the last gamma
         gamma = np.hstack((gamma,
                            np.sum(xi[:, :, T - 2], axis=0).reshape((-1, 1))))
 
